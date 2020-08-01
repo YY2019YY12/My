635ea40786277893f516d3e6912325cfa160bf75
Your Name <you@exampl.com>
2020-08-01 11:11:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mywview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mywview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=findViewById(R.id.we);
+    }
+}
